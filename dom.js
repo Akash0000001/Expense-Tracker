0c8547375c610ca9ed576096af9540dfa847241d
Akash0000001 <akashranjan947@gmail.com>
2023-04-27 04:32:23 +0530

    Dom manipulation

---
+++ dom.js
@@ -0,0 +1,18 @@
+console.log(document)
+console.log(document.URL)
+console.log(document.domain);
+document.title="list of item";
+console.log(document.doctype);
+console.log(document.body);
+console.log(document.head);
+console.log(document.all);
+console.log(document.forms);
+console.log(document.images);
+const header=document.getElementById("main-header");
+header.style.border="3px solid black";
+const titles=document.getElementsByClassName("title");
+titles[0].style.fontWeight="bold";
+titles[0].style.color="green";
+
+
+
