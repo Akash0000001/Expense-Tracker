4060bbaab336ccf9aa538dc029fd60ad5b84f8f7
Akash0000001 <akashranjan947@gmail.com>
2023-04-27 04:39:28 +0530

    updated Dom Manipulation

--- dom.js
+++ dom.js
@@ -9,7 +9,7 @@ console.log(document.all);
 console.log(document.forms);
 console.log(document.images);
 const header=document.getElementById("main-header");
-header.style.border="3px solid black";
+header.style.border="4px solid black";
 const titles=document.getElementsByClassName("title");
 titles[0].style.fontWeight="bold";
 titles[0].style.color="green";
